d0792f85daed3409a8d19120ff6b8fd6e9123dd4
siwakon.dev <sc.sivakorn@gmail.com>
2024-05-04 15:57:23 +0700

    add config k8s

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,8 @@
 # create dockerhub-token
 # change demo to your account
-kubectl create secret docker-registry dockerhub-registrykey --docker-username=demo --docker-password=demo --docker-email=demo@gmail.com --namespace=demo-web
+kubectl create secret docker-registry dockerhub-registrykey --docker-username=demo --docker-password=demo --docker-email=demo@gmail.com --namespace=bazooka168
 
 # create env to secret
-kubectl create secret generic demo-web-env --namespace=demo --from-env-file=.env
+kubectl create secret generic bazooka168-member-env --namespace=bazooka168 --from-env-file=.env
+kubectl create secret generic bazooka168-backoffice-env --namespace=bazooka168 --from-env-file=.env
+kubectl create secret generic bazooka168-api-env --namespace=bazooka168 --from-env-file=.env
